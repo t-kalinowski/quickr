ed6f6d84175d76e37f6c9219de84600d7d2c9d22
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-05-13 09:24:10 -0400

    fix auto return value

--- R/preprocess-lang.R
+++ R/preprocess-lang.R
@@ -18,7 +18,7 @@ ensure_last_expr_sym <- function(bdy) {
     stop("bad body, needs {")
   if (!is.symbol(last_expr <- last(bdy))) {
     bdy[[length(bdy)]] <- call("<-", quote(out_), last_expr)
-    bdy[[length(bdy + 1L)]] <- quote(out_)
+    bdy[[length(bdy) + 1L]] <- quote(out_)
   }
   bdy
 }
