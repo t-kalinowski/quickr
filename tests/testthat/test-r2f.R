411f75bcbfb39bfae7b14b6e1295f04be7d37339
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-05-18 15:46:43 -0400

    fixes and tests for `ifelse`
    closes #10

--- tests/testthat/test-r2f.R
+++ tests/testthat/test-r2f.R
@@ -795,3 +795,30 @@ test_that("complex unary intrinsics", {
   }
 
 })
+
+
+test_that("ifelse", {
+  fn <- function(a, b) {
+    declare(
+      type(a = integer(n)),
+      type(b = integer(n))
+    )
+
+    out <- ifelse(a < b, 1, -1)
+    out
+  }
+  expect_quick_identical(fn, list(-10:10, integer(21)))
+
+  # double version of above
+  fn <- function(a, b) {
+    declare(
+      type(a = double(n)),
+      type(b = double(n))
+    )
+
+    out <- ifelse(a < b, 1, -1)
+    out
+  }
+  expect_quick_equal(fn, list(seq(-5, 5, length.out = 20), double(20)))
+
+})
