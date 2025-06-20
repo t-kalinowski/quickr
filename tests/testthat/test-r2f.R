4ade26cf7d2e4acf02eb89a955a7be98eb9d2cba
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-06-20 12:00:05 -0400

    add tests

--- tests/testthat/test-r2f.R
+++ tests/testthat/test-r2f.R
@@ -981,3 +981,55 @@ test_that("while + break", {
   expect_translation_snapshots(inc_to_5_break_while)
   expect_quick_identical(inc_to_5_break_while, -1L, 0L, 4L, 5L)
 })
+
+test_that("expr return value", {
+  fn <- function(x) {
+    declare(type(x = integer(NA)))
+    x + 1L
+  }
+
+  expect_translation_snapshots(fn)
+  expect_quick_identical(fn, 1:10)
+})
+
+
+# -------------------------------------------------------------------------
+# Remainder (%%) and integer division (%/%) operators
+# -------------------------------------------------------------------------
+
+test_that("%% and %%", {
+  x <- -7:7
+  x <- expand.grid(a = x, b = x)
+  x <- x[x$b != 0, ]                  # avoid divisor == 0
+  x <- as.list(x)
+
+  rem_double <- function(a, b) {
+    declare(type(a = double(n)), type(b = double(n)))
+    a %% b
+  }
+
+  expect_translation_snapshots(rem_double)
+  expect_quick_equal(rem_double, lapply(x, function(v) v + 0.2))
+
+  rem_int <- function(a, b) {
+    declare(type(a = integer(n)), type(b = integer(n)))
+    a %% b
+  }
+  expect_quick_identical(rem_int, x)
+
+
+  div_double <- function(a, b) {
+    declare(type(a = double(n)), type(b = double(n)))
+    a %/% b
+  }
+
+  expect_translation_snapshots(div_double)
+  expect_quick_equal(div_double, lapply(x, function(v) v + 0.2))
+
+  div_int <- function(a, b) {
+    declare(type(a = integer(n)), type(b = integer(n)))
+    a %/% b
+  }
+  expect_quick_identical(div_int, x)
+})
+
