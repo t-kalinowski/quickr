8147c3de1ef45323354b2a5135c706f985bd6d9f
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-12-30 18:29:27 -0500

    Add seq coverage tests

--- tests/testthat/test-seq.R
+++ tests/testthat/test-seq.R
@@ -14,6 +14,56 @@ test_that("seq", {
   expect_quick_identical(fn, list(-1L, 10L), list(10L, -5L))
 })
 
+test_that("seq supports literal bounds and steps", {
+  seq_range <- function() {
+    out <- seq(2L, 5L)
+    out
+  }
+
+  seq_step <- function() {
+    out <- seq(1L, 5L, by = 2L)
+    out
+  }
+
+  expect_quick_identical(seq_range, list())
+  expect_quick_identical(seq_step, list())
+})
+
+test_that("seq_along returns linear indices for arrays", {
+  seq_along_matrix <- function(x) {
+    declare(type(x = double(m, n)))
+    out <- seq_along(x)
+    out
+  }
+
+  expect_quick_identical(
+    seq_along_matrix,
+    list(matrix(as.double(1:6), nrow = 2))
+  )
+})
+
+test_that("seq rejects length.out and along.with", {
+  bad_length_out <- function() {
+    out <- seq(1L, 5L, length.out = 3L)
+    out
+  }
+
+  bad_along_with <- function(x) {
+    declare(type(x = double(NA)))
+    out <- seq(along.with = x)
+    out
+  }
+
+  expect_error(
+    quick(bad_length_out),
+    regexp = "seq\\(length.out=, along.with=\\) not implemented yet"
+  )
+  expect_error(
+    quick(bad_along_with),
+    regexp = "seq\\(length.out=, along.with=\\) not implemented yet"
+  )
+})
+
 test_that("seq-like iterables work in subset indexing", {
   seq_index <- function(x, start, end) {
     declare(
